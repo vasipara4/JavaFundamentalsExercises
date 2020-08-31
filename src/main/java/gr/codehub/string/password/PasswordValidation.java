3a573afad7406e10acf4f584af50058be757e831
vasipara4 <billprig@gmail.com>
2020-08-31 22:24:07 +0300

    Fix RegEx patterns

--- src/main/java/gr/codehub/string/password/PasswordValidation.java
+++ src/main/java/gr/codehub/string/password/PasswordValidation.java
@@ -8,41 +8,55 @@ public class PasswordValidation {
 
     //Constants Declaration
     private static final int MIN_PASSWORD_LENGTH = 8;
+    //Password length = 8 is mandatory, so the remaining criteria are 2
     private static final int MIN_CRITERIA = 2;
-    //Password length: 8 is mandatory, so the remaining criteria are 2
+
 
 
     protected static boolean existUppercase(String str) {
         String pattern = ".*(?=.*[A-Z]).*";
-//        System.out.println(str.matches(pattern));
+        //System.out.println(str.matches(pattern));
         return str.matches(pattern);
     }
 
     protected static boolean existLowercase(String str) {
         String pattern = ".*(?=.*[a-z]).*";
-//        System.out.println(str.matches(pattern));
+        //System.out.println(str.matches(pattern));
         return str.matches(pattern);
     }
 
     protected static boolean existNumber(String str) {
         String pattern = ".*(?=.*[0-9]).*";
-//        System.out.println(str.matches(pattern));
+        //System.out.println(str.matches(pattern));
         return str.matches(pattern);
     }
 
     protected static boolean existSpecialChar(String str) {
         String pattern = ".*(?=.*[@#$%^&+=]).*";
-//        System.out.println(str.matches(pattern));
+        //System.out.println(str.matches(pattern));
         return str.matches(pattern);
     }
 
 
-    //    Return true if there is no 3 sequence characters (same or sequential ascii characters)
+    //    Returns TRUE if there is NOT a sequence of 3 identical/sequential characters
+    //    (identical CASE_INSENSITIVE or sequential ascii characters)
     protected static boolean noSequenceCharacters(String str) {
         int len = str.length();
-        String patternSameCharacters = "(?i)(?:([a-z0-9])\\1{2,})*";
-        final Pattern p = Pattern.compile(patternSameCharacters);
+
+        //Below string - pattern tested and does not work properly
+        //String patternSameCharacters = "(?i)(?:([a-z0-9])\\1{2,})*";
+        //final Pattern p = Pattern.compile(patternSameCharacters);
+
+        //Pattern for 3 or more consecutive identical characters/numbers
+        Pattern p = Pattern.compile("([a-z\\d])\\1\\1", Pattern.CASE_INSENSITIVE);
         final Matcher m = p.matcher(str);
+
+        //Detect 3 same characters in sequence
+        if (m.find()) {
+            System.out.println("Match");
+            return false;
+        }
+
 
         // Detect 3 sequential ASCII characters (Only A-Za-z characters)
         //        ASCII chars to numbers: A-> 65, X->88 , a->97, x->120
@@ -52,17 +66,10 @@ public class PasswordValidation {
             int thirdStepChar = str.charAt(i + 2);
 
             if ((firstStepChar >= 65 && firstStepChar <= 88) || (firstStepChar >= 97 && firstStepChar <= 120)) {
-                if (firstStepChar == secondStepChar - 1 && secondStepChar == thirdStepChar - 1) {
-                    // System.out.println("sequence characters");
+                if (firstStepChar == secondStepChar - 1 && secondStepChar == thirdStepChar - 1)
                     return false;
-                }
             }
         }
-
-        //Detect 3 same characters in sequence
-        if (m.find())
-            return false;
-
 
         return true;
 
